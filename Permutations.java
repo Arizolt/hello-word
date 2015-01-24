cf16dcb17f08ddb17b2f1962bbc93b89e115cb01
Arizolt <Arizolt@users.noreply.github.com>
2015-01-24 10:14:16 +0300

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -1,5 +1,27 @@
-public class Permutations {
-	public static void main(String[] args) {
-		System.out.println("Hello word");
+import java.util.Scanner;
+
+class Permutation {
+
+	String set;
+	
+	public static void lex (int pos) {
+		if (pos == len) {
+			
+		}
 	}
+	
+	public static void main (String Args[]) {
+		
+		Scanner input = new Scanner(System.in);	
+		
+		Permutation MyWord = new Permutation();
+		
+		System.out.println("Введите строку");
+		
+		MyWord.set = input.nextLine();
+		
+		System.out.println("\nВведеная строка: " + MyWord.set);
+	
+	}
+	
 }
