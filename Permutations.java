c54feec07d855871a8888934f412bf239009022b
Arizolt <Arizolt@users.noreply.github.com>
2015-01-26 17:57:41 +0300

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 
 class Permutation {
 
-	String set;
+	String word;
 	
-	public static void lex (int pos) {
-		if (pos == len) {
+	public static String findNextPerm (String word) {
+		
+		int i = word.length() - 2, 
+			j = word.length() - 1;
+		
+		while ( ( i >= 0 ) && ( word.charAt(i) >= word.charAt(i + 1) ) ) {
+			
+			i--;
 			
 		}
+		
+		System.out.println("\nНомер i = " + i);
+		
+		while ( ( j >= 0 ) && ( word.charAt(j) <= word.charAt(i) ) ) {
+			
+			j--;
+			
+		}
+		
+		System.out.println("\nНомер j = " + j);
+				
+		return "ыыы";
 	}
 	
 	public static void main (String Args[]) {
@@ -18,13 +36,19 @@ class Permutation {
 		
 		System.out.println("Введите строку");
 		
-		MyWord.set = input.nextLine();
+		MyWord.word = input.nextLine();
 		
-		if ( MyWord.set.length() != 12 ) {
+		if ( MyWord.word.length() != 5 ) {
+			
 			System.out.println("Неверно введена строка");
+			
 		} else {
-			System.out.println("\nВведеная строка: " + MyWord.set);
+			
+			System.out.println("\nВведеная строка: " + MyWord.word);
+			
 		}
+		
+		MyWord.findNextPerm(MyWord.word);
 	
 	}
 	
